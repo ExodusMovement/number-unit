4479ec14a1b5c62372439c8baeda38fe01397066
JP Richardson <jprichardson@gmail.com>
2015-11-07 16:31:08 -0600

    tests: test ZERO of UnitType instance

---
+++ tests/unit-type_zero.test.js
@@ -0,0 +1,14 @@
+import test from 'tape'
+import { bitcoin, dollar } from './fixtures'
+
+test('ZERO should return a number-unit with value of 0 in default unit', function (t) {
+  const b0 = bitcoin.ZERO
+  t.equal(b0.toNumber(), 0, 'bitcoin value is 0')
+  t.equal(b0.unit.toString(), 'bits', 'unit is default unit (bits)')
+
+  const d0 = dollar.ZERO
+  t.equal(d0.toNumber(), 0, 'dollar value is 0')
+  t.equal(d0.unit.toString(), 'USD', 'unit is default unit (USD)')
+
+  t.end()
+})
