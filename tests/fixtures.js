b96abe36abdee6d1aa381ac11f120dc082d1a80d
JP Richardson <jprichardson@gmail.com>
2015-11-08 09:40:31 -0600

    test/fixtures: added distance

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -13,3 +13,20 @@ export const dollar = UnitType.create('dollar', fiatCurrency, {
   USD: 1,
   cents: 1e-2
 }, 'USD')
+
+export const distance = UnitType.create('distance')
+export const distanceUS = UnitType.create('distance-us', distance, {
+  miles: 5280,
+  yards: 3,
+  feet: 1,
+  inches: 1 / 12
+}, 'feet')
+
+export const distanceSI = UnitType.create('distance-si', distance, {
+  km: 1e3,
+  m: 1,
+  cm: 1e-2,
+  mm: 1e-3,
+  um: 1e-6,
+  nm: 1e-9
+}, 'm')
