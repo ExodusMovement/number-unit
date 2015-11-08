4b70b8e52508f6f00059d8894757e34b3f664e9d
JP Richardson <jprichardson@gmail.com>
2015-11-08 10:21:23 -0600

    tests: equals() tests

---
+++ tests/number-unit_equals.test.js
@@ -0,0 +1,14 @@
+import test from 'tape'
+import { bitcoin, distanceSI } from './fixtures'
+
+test('equals() should return true if any number with unit is equivalent', function (t) {
+  t.true(bitcoin.BTC('1.53').equals(bitcoin.bits(1530000)), '1.53 BTC === 1530000 bits')
+  t.false(bitcoin.BTC('1.53').equals(bitcoin.bits(1530001)), '1.53 BTC !== 1530001 bits ')
+
+  t.true(distanceSI.km(3.5).equals(distanceSI.m(3500)), '3.5 km === 3500 m')
+  t.false(distanceSI.km(3.5).equals(distanceSI.m(3500.1)), '3.5 km !== 3500.1 m')
+
+  t.true(distanceSI.parse('3.5 km').equals(distanceSI.parse('3500 m')), '3.5 km === 3500 m (from parse)')
+
+  t.end()
+})
