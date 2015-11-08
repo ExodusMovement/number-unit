5fd048acb7f99734a68a76b56ca774697ce5162e
JP Richardson <jprichardson@gmail.com>
2015-11-08 00:04:36 -0600

    tests: to(), toString(), toNumber() conversions

---
+++ tests/number-unit_to.test.js
@@ -0,0 +1,41 @@
+import test from 'tape'
+import { bitcoin } from './fixtures'
+
+test('to() other units', function (t) {
+  let b1 = bitcoin.BTC(1.53)
+
+  let b2 = b1.to(bitcoin.bits)
+  let b3 = b1.to('bits')
+
+  t.equals(b2.toString(), '1530000 bits', 'number w/unit string')
+  t.equals(b3.toString(), '1530000 bits', 'number w/unit string')
+
+  t.end()
+})
+
+test('toString() with unit parameter', function (t) {
+  let b1 = bitcoin.BTC(1.53)
+
+  t.equals(b1.toString(), '1.53 BTC', 'toString() w/ no param')
+  t.equals(b1.toString({ unit: false }), '1.53', 'toString() w/ unit param false')
+  t.equals(b1.toString({ unit: true }), '1.53 BTC', 'toString() w/ unit param true')
+
+  t.end()
+})
+
+test('toString() with format parameter', function (t) {
+  let b1 = bitcoin.BTC(1.53)
+
+  t.equals(b1.toString({ format: (value, unit) => unit + ' ' + value }), 'BTC 1.53', 'reverse unit & value')
+
+  t.end()
+})
+
+test('toNumber()', function (t) {
+  let b1 = bitcoin.BTC(1.53)
+
+  t.equals(b1.toNumber(), 1.53)
+  t.equals(b1.to('bits').toNumber(), 1530000)
+
+  t.end()
+})
