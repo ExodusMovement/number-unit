a34e2fde6741fec08fb9c7323826b7ce18096ed5
JP Richardson <jprichardson@gmail.com>
2015-11-08 00:04:48 -0600

    tests: JSON conversions

---
+++ tests/number-unit_json.test.js
@@ -0,0 +1,25 @@
+import test from 'tape'
+import { bitcoin } from './fixtures'
+
+test('JSON stringify should not throw an error', function (t) {
+  let b1 = bitcoin.BTC(1.53)
+  let json = JSON.stringify(b1)
+  t.ok(json, 'JSON stringify did not throw an error')
+
+  t.end()
+})
+
+test('toJSON()', function (t) {
+  let b1 = bitcoin.BTC('-1.53')
+
+  let json = b1.toJSON()
+
+  t.same(json, {
+    value: '-1.53',
+    unit: 'BTC',
+    unitType: 'currency/crypto-currency/bitcoin',
+    type: 'NumberUnit'
+  })
+
+  t.end()
+})
