fc0ed1ac1aced9c65d053c706a4561041ebfea5d
JP Richardson <jprichardson@gmail.com>
2015-11-07 23:15:34 -0600

    test: inspect()

---
+++ tests/number-unit_inspect.test.js
@@ -0,0 +1,11 @@
+import test from 'tape'
+import { inspect } from 'util'
+import { bitcoin } from './fixtures'
+
+test('inspect()', function (t) {
+  let b1 = bitcoin.BTC(1.53)
+  let i = inspect(b1)
+  t.same(i, '<NumberUnit: 1.53 BTC >', 'should return a string for console.dir / inspect')
+
+  t.end()
+})
